51d57e2eb506abe55e5a1f065ed7ee417ef18c2c
FlipSiteX <verapushtrinova@gmail.com>
2023-07-15 01:14:51 +0300

    ns

--- app.js
+++ app.js
@@ -2,23 +2,28 @@ const inpElement = document.getElementById('title')
 const addBtn = document.getElementById('add')
 const listElement = document.getElementById('list')
 
-const notes = ['Хлеб', 'Батон']
+const notes = [
+    {title: 'Хлеб',
+    completed: true},
+    {title: 'Батон',
+    completed: false}
+]
 
 function render() {
-
+    listElement.innerHTML = ''
     for (let i = 0; i < notes.length; i++) {
-        listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i]))
+        listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i], i))
     }
 }
 
 render()
 
-function getNoteTemplate (title) {
+function getNoteTemplate (note, index) {
     return    `
     <li>
-         <span>${title}</span>
+         <span class='${note.completed ? 'line-through' : ''}'>${note.title}</span>
         <span>
-            <span id="check"><i class="fa-solid fa-check"></i></span>
+            <span id="check" class='${note.completed ? 'done' : 'not-done-check'}' data-index='${index}'><i class="fa-solid fa-check"></i></span>
             <span id="delete"><i class="fa-solid fa-xmark"></i></span>
         </span>
      </li>
@@ -29,6 +34,11 @@ addBtn.onclick = function () {
     if (inpElement.value === '') {
         return 
     }
-    listElement.insertAdjacentHTML('beforeend', getNoteTemplate(inpElement.value))
+    const newNote = {
+        title: inpElement.value,
+        completed: false
+    }
+    notes.push(newNote)
+    render()
     inpElement.value = ''
 }
