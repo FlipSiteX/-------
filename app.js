1c13e00333e5b26232c13c0cad0fe3f63f1bddec
FlipSiteX <verapushtrinova@gmail.com>
2023-07-15 22:35:33 +0300

    ez

--- app.js
+++ app.js
@@ -2,15 +2,13 @@ const inpElement = document.getElementById('title')
 const addBtn = document.getElementById('add')
 const listElement = document.getElementById('list')
 
-const notes = [
-    {title: 'Хлеб',
-    completed: true},
-    {title: 'Батон',
-    completed: false}
-]
+const notes = []
 
 function render() {
     listElement.innerHTML = ''
+    if (notes.length === 0) {
+        listElement.innerHTML = '<li>Список пуст</li>'
+    }
     for (let i = 0; i < notes.length; i++) {
         listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i], i))
     }
@@ -23,8 +21,8 @@ function getNoteTemplate (note, index) {
     <li>
          <span class='${note.completed ? 'line-through' : ''}'>${note.title}</span>
         <span>
-            <span id="check" class='${note.completed ? 'done' : 'not-done-check'}' data-index='${index}'><i class="fa-solid fa-check"></i></span>
-            <span id="delete"><i class="fa-solid fa-xmark"></i></span>
+            <i class="fa-solid fa-check ${note.completed ? 'done' : 'not-done'}" id="check" data-index='${index}' data-type='switch'></i>
+            <i class="fa-solid fa-xmark" id="delete" data-index='${index}' data-type='remove'></i>
         </span>
      </li>
      `
@@ -41,4 +39,18 @@ addBtn.onclick = function () {
     notes.push(newNote)
     render()
     inpElement.value = ''
+}
+
+listElement.onclick = function (event) {
+    if (event.target.dataset.index) {
+        const index = Number(event.target.dataset.index)
+        const type = event.target.dataset.type
+        if (type === 'switch') {
+            notes[index].completed = !notes[index].completed
+        }
+        else {
+            notes.splice(index, 1)
+        }
+        render()
+    }
 }
